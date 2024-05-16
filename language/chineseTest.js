4b47612d40416684de6fdde0b699e74ca45cf612
ctrl-acvh <ctrl_h_macro@proton.me>
2024-05-16 11:33:56 -0700

    update

---
+++ language/chineseTest.js
@@ -0,0 +1,31 @@
+function chineseList()
+{
+ replacements=
+			[
+				[" and ","<ruby><rb> and </rb><rt>和Hé</rt></ruby>"],
+				[" to ","<ruby><rb> to </rb><rt>至zhì</rt></ruby>"],
+				[" a ","<ruby><rb> a </rb><rt>一种yī zhǒng</rt></ruby>"],
+				[" in ","<ruby><rb> in </rb><rt>在zài</rt></ruby>"],
+				[" is ","<ruby><rb> is </rb><rt>是shì</rt></ruby>"],
+				[" you ","<ruby><rb> you </rb><rt>您nín</rt></ruby>"],
+				[" are ","<ruby><rb> are </rb><rt>是shì</rt></ruby>"],
+				[" for ","<ruby><rb> for </rb><rt>对于duìyú</rt></ruby>"],
+				[" that ","<ruby><rb> that </rb><rt>那nà</rt></ruby>"],
+				[" or ","<ruby><rb> or </rb><rt>要么yàome</rt></ruby>"],
+				[" it ","<ruby><rb> it </rb><rt>它tā</rt></ruby>"],
+				[" as ","<ruby><rb> as </rb><rt>如rú</rt></ruby>"],
+				[" be ","<ruby><rb> be </rb><rt>是shì</rt></ruby>"],
+				[" on ","<ruby><rb> on </rb><rt>上shàng</rt></ruby>"],
+				[" your ","<ruby><rb> your </rb><rt>你的nǐ de</rt></ruby>"],
+				[" with ","<ruby><rb> with </rb><rt>与yǔ</rt></ruby>"],
+				[" can ","<ruby><rb> can </rb><rt>能够nénggòu</rt></ruby>"],
+				[" have ","<ruby><rb> have </rb><rt>有yǒu</rt></ruby>"],
+				[" this ","<ruby><rb> this </rb><rt>这个zhège</rt></ruby>"],
+				[" an ","<ruby><rb> an </rb><rt>一个yīgè</rt></ruby>"],
+				[" by ","<ruby><rb> by </rb><rt>通过tōngguò</rt></ruby>"],
+				[" not ","<ruby><rb> not </rb><rt>不bù</rt></ruby>"],
+				[" but ","<ruby><rb> but </rb><rt>但dàn</rt></ruby>"],
+				[" at ","<ruby><rb> at </rb><rt>在zài</rt></ruby>"],
+				[" from ","<ruby><rb> from </rb><rt>从cóng</rt></ruby>"]
+				]
+}
